24621421cc99a30851bedcb74cb35cb4cfef5795
Gsx <2249908337@qq.com>
2020-04-21 21:31:27 +0800

    First commit

---
+++ GitTest/GitProject/src/main/java/com/gsx/pojo/User.java
@@ -0,0 +1,22 @@
+package com.gsx.pojo;
+
+public class User {
+    private Integer Id;
+    private String name;
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public void setId(Integer id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
