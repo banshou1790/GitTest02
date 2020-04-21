e46b1d02b8fd20cff0466cbbc4b38b3d85126c01
Gsx <2249908337@qq.com>
2020-04-21 21:37:08 +0800

    add "birthday"

--- GitTest/GitProject/src/main/java/com/gsx/pojo/User.java
+++ GitTest/GitProject/src/main/java/com/gsx/pojo/User.java
@@ -1,8 +1,19 @@
 package com.gsx.pojo;
 
+import javax.xml.crypto.Data;
+
 public class User {
     private Integer Id;
     private String name;
+    private Data birthday;
+
+    public Data getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Data birthday) {
+        this.birthday = birthday;
+    }
 
     public Integer getId() {
         return Id;
